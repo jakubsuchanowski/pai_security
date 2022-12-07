f9a425f84a1cab1cbb3a50db296ad1d0d079e6f9
Jakub <kubasuchanowski@gmail.com>
2022-12-07 16:55:12 +0100

    blad przy usuwaniu edytowanego

--- src/main/java/com/js/pai_security/controller/UserController.java
+++ src/main/java/com/js/pai_security/controller/UserController.java
@@ -6,13 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
 import java.security.Principal;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
 
 
 @Controller
@@ -29,13 +31,27 @@ public class UserController {
     }
     @GetMapping("/register")
     public String registerPage(Model m) {
+
         //dodanie do modelu nowego użytkownika
         m.addAttribute("user", new User());
         //zwrócenie nazwy widoku rejestracji - register.html
         return "register";
     }
     @PostMapping("/register")
-    public String registerPagePOST(@ModelAttribute User user) {
+    public String registerPagePOST(@Valid User user, BindingResult binding) {
+
+
+        if (dao.findByLogin(user.getLogin()) != null) {
+            binding.rejectValue("login", "error.users", "Istnieje już użytkownik o takim loginie");
+        }
+        if (binding.hasErrors()) {
+            return "register";
+        }
+
+//        if (dao.findByLogin(user.getName()) != null) {
+//            binding.rejectValue("login", "Login nie może się powtarzać");
+//            return "register";
+//        }
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         dao.save(user);
         //przekierowanie do adresu url: /login
@@ -52,14 +68,38 @@ public class UserController {
     public String usersList(Model m) {
         //definicja metody, która zwróci do widoku users.html listę
         //użytkowników z bd
-            m.addAttribute("Nazwa",dao.findAll());
+            m.addAttribute("lista",dao.findAll());
         return "users";
 
     }
 
-    @PostMapping("/editUser")
-    public String editUser(){
-        return "userEdit";
+    @GetMapping("/delete")
+    public String delete(Principal principal){
+       dao.delete(dao.findByLogin(principal.getName()));
+       return "redirect:/logout";
+    }
+
+    @GetMapping("/edit")
+    public String showEditForm(Principal principal, Model model){
+        model.addAttribute("user",dao.findByLogin(principal.getName()));
+        return "editUser";
+    }
+    @PostMapping("/update")
+    public String update(@Valid User user, BindingResult bindingResult, Principal principal){
+//        model.addAttribute("user", dao.findByLogin(principal.getName()));
+//        User currentUser = dao.findByLogin(principal.getName());
+        if(dao.findByLogin(user.getLogin()) != null && !Objects.equals(user.getLogin(), principal.getName())){
+            bindingResult.rejectValue("login", "error.users", "Istnieje już użytkownik o podanym loginie!");
+        }
+        if(bindingResult.hasErrors()){
+            return "editUser";
+        }
+        user.setName(user.getName());
+        user.setSurname(user.getSurname());
+        user.setLogin(user.getLogin());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        dao.save(user);
+        return "redirect:/users";
     }
 }
 
--- src/main/java/com/js/pai_security/entity/User.java
+++ src/main/java/com/js/pai_security/entity/User.java
@@ -1,8 +1,14 @@
 package com.js.pai_security.entity;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+
 
 @Entity
 @Table(name = "Users")
@@ -12,9 +18,20 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer userid;
+
+
+    @NotEmpty(message = "Imie musi być uzupełnione!")
+    @Pattern(regexp = "[a-zA-Z]{2,30}", message="Podaj poprawnie imie!!!")
     private String name;
+    @NotEmpty(message = "Nazwisko musi być uzupełnione!")
+    @Pattern(regexp = "[a-zA-Z]{2,30}", message="Podaj poprawnie nazwisko!!!")
     private String surname;
+
+    @NotEmpty(message = "Login musi być uzupełniony!")
+    @Size(min = 3, message = "Wpisz co najmniej 3 znaki!")
     private String login;
+    @NotEmpty(message = "Hasło musi być uzupełnione!")
+    @Size(min = 3, message = "Wpisz co najmniej 3 znaki!")
     private String password;
 
     public User(String name, String surname, String login, String password){
